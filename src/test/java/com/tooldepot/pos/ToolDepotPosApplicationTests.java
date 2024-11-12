effab6538c5372d55be2c97d468c16d437e19f27
Troy Landers <tlanders@landersconsulting.biz>
2024-11-12 16:29:22 -0600

    initial commit

---
+++ src/test/java/com/tooldepot/pos/ToolDepotPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tooldepot.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolDepotPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
