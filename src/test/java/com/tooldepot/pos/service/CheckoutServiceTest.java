79bbe4008281772c2191b78a37154eb1670cb8e8
Troy Landers <tlanders@landersconsulting.biz>
2024-11-15 15:33:01 -0600

    added mockbeans so can better test checkout service

--- src/test/java/com/tooldepot/pos/service/CheckoutServiceTest.java
+++ src/test/java/com/tooldepot/pos/service/CheckoutServiceTest.java
@@ -1,17 +1,20 @@
 package com.tooldepot.pos.service;
 
-import com.tooldepot.pos.domain.RentalTransaction;
-import com.tooldepot.pos.domain.Tool;
-import com.tooldepot.pos.domain.ToolType;
+import com.tooldepot.pos.domain.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static com.tooldepot.pos.util.BigDecimalUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Slf4j
@@ -19,34 +22,58 @@ public class CheckoutServiceTest {
     @Autowired
     private CheckoutService checkoutService;
 
+    @MockBean
+    private ToolService toolService;
+
+    @MockBean
+    private RentalPeriodService rentalPeriodService;
+
+//    @MockBean
+//    private PricingService pricingService;
+
     @Test
     public void testValidCheckout() throws PosServiceException {
         log.info("Testing valid checkout");
 
-        Tool testTool = new Tool("LADW", ToolType.LADDER, "Werner");
+        testValidCheckout(new Tool("LADW", ToolType.LADDER, "Werner"),
+                new RentalPeriod(3, 3, LocalDate.now(), LocalDate.now().plusDays(3)),
+                0);
+        testValidCheckout(new Tool("LADW", ToolType.LADDER, "Werner"),
+                new RentalPeriod(3, 3, LocalDate.now(), LocalDate.now().plusDays(3)),
+                10);
 
-        testValidCheckout(testTool, 3, 0, LocalDate.now());
-
-        checkoutService.checkout("LADW", 1, 0, LocalDate.now());
-        checkoutService.checkout("LADW", 3, 10, LocalDate.now());
-        checkoutService.checkout("LADW", 7, 53, LocalDate.now());
+//        checkoutService.checkout(expectedTool.toolCode(), 1, 0, LocalDate.now());
+//        checkoutService.checkout(expectedTool.toolCode(), 3, 10, LocalDate.now());
+//        checkoutService.checkout(expectedTool.toolCode(), 7, 53, LocalDate.now());
     }
 
-    private void testValidCheckout(Tool testTool, int rentalDays, int discountPercent, LocalDate checkoutDate) throws PosServiceException {
-        RentalTransaction rental = checkoutService.checkout(testTool.toolCode(), rentalDays, discountPercent, checkoutDate);
+    private void testValidCheckout(Tool expectedTool, RentalPeriod expectedRentalPeriod, int discountPercent) throws PosServiceException {
+        when(toolService.getTool(any())).thenReturn(Optional.of(expectedTool));
+        when(rentalPeriodService.getRentalPeriod(any(), any(), anyInt()))
+                .thenReturn(expectedRentalPeriod);
+
+        RentalTransaction rental = checkoutService.checkout(expectedTool.toolCode(), expectedRentalPeriod.rentalDays(),
+                discountPercent, expectedRentalPeriod.checkoutDate());
+        BigDecimal expectedPreDiscountCharge = multiply(expectedTool.toolType().getDailyCharge(), expectedRentalPeriod.rentalDays(), 2);
+        BigDecimal expectedDiscountAmount = multiply(expectedPreDiscountCharge,
+                divide(discountPercent, BD_100, 2), 2);
+        BigDecimal expectedFinalCharge = expectedPreDiscountCharge.subtract(expectedDiscountAmount);
+
         assertAll("rental",
                 () -> assertNotNull(rental),
                 () -> assertNotNull(rental.tool()),
-                () -> assertEquals(testTool.toolCode(), rental.tool().toolCode()),
-                () -> assertEquals(testTool.toolType(), rental.tool().toolType()),
-                () -> assertEquals(testTool.brand(), rental.tool().brand()),
-                () -> assertEquals(rentalDays, rental.rentalDays()),
-                () -> assertEquals(checkoutDate, rental.checkoutDate()),
-                () -> assertEquals(checkoutDate.plusDays(rentalDays), rental.dueDate()),
-                () -> assertEquals(testTool.toolType().getDailyCharge(), rental.dailyRentalCharge()),
-                () -> assertEquals(rentalDays, rental.chargeDays()),
-                () -> assertEquals(multiply(testTool.toolType().getDailyCharge(), rentalDays, 2), rental.preDiscountCharge()),
-                () -> assertEquals(discountPercent, rental.discountPercent())
+                () -> assertEquals(expectedTool.toolCode(), rental.tool().toolCode()),
+                () -> assertEquals(expectedTool.toolType(), rental.tool().toolType()),
+                () -> assertEquals(expectedTool.brand(), rental.tool().brand()),
+                () -> assertEquals(expectedRentalPeriod.rentalDays(), rental.rentalDays()),
+                () -> assertEquals(expectedRentalPeriod.checkoutDate(), rental.checkoutDate()),
+                () -> assertEquals(expectedRentalPeriod.returnDate(), rental.dueDate()),
+                () -> assertEquals(expectedTool.toolType().getDailyCharge(), rental.dailyRentalCharge()),
+                () -> assertEquals(expectedRentalPeriod.chargeDays(), rental.chargeDays()),
+                () -> assertEquals(expectedPreDiscountCharge, rental.preDiscountCharge()),
+                () -> assertEquals(discountPercent, rental.discountPercent()),
+                () -> assertEquals(expectedDiscountAmount, rental.discountAmount()),
+                () -> assertEquals(expectedFinalCharge, rental.finalCharge())
         );
     }
 
