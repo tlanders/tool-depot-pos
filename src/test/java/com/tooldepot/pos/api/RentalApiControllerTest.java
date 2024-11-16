61e394f74da13d5f6e1e91bb20b10e00eb3ef7f8
Troy Landers <tlanders@landersconsulting.biz>
2024-11-16 14:56:42 -0600

    finished checkout api tests

--- src/test/java/com/tooldepot/pos/api/RentalApiControllerTest.java
+++ src/test/java/com/tooldepot/pos/api/RentalApiControllerTest.java
@@ -2,6 +2,7 @@ package com.tooldepot.pos.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tooldepot.pos.api.RentalApiController.CheckoutRequest;
+import com.tooldepot.pos.service.PosServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,8 +30,8 @@ public class RentalApiControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    public void testCheckout() throws Exception {
-        log.info("testCheckout");
+    public void testValidCheckout() throws Exception {
+        log.info("testValidCheckout");
 
         CheckoutRequest request = new CheckoutRequest("LADW", 3, 0,
                 LocalDate.of(2024, 11, 1));
@@ -44,12 +45,53 @@ public class RentalApiControllerTest {
                 .andExpect(jsonPath("$.checkoutDate", equalTo("11/01/24")));
     }
 
-//    @Test
-//    public void testGetTool_unknownCode() throws Exception{
-//        log.info("testGetTool_unknownCode");
-//
-//        mockMvc.perform(get("/api/tools/ZZZZ")
-//                .contentType("application/json"))
-//                .andExpect(status().isNotFound());
-//    }
+    @Test
+    public void testCheckout_invalidRentalDays() throws Exception {
+        log.info("testCheckout_invalidRentalDays");
+
+        testInvalidCheckout("LADW", -3, 0,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_RENTAL_DAYS.getErrorCode());
+        testInvalidCheckout("LADW", 0, 0,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_RENTAL_DAYS.getErrorCode());
+    }
+
+    @Test
+    public void testCheckout_invalidToolCode() throws Exception {
+        log.info("testCheckout_invalidToolCode");
+
+        testInvalidCheckout("ABC", 4, 0,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_TOOL_CODE.getErrorCode());
+        testInvalidCheckout("", 5, 0,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_TOOL_CODE.getErrorCode());
+    }
+
+    @Test
+    public void testCheckout_invalidDiscount() throws Exception {
+        log.info("testCheckout_invalidDiscount");
+
+        testInvalidCheckout("LADW", 4, -1,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_DISCOUNT_PERCENTAGE.getErrorCode());
+        testInvalidCheckout("LADW", 5, 101,
+                LocalDate.of(2024, 11, 1),
+                PosServiceException.Error.INVALID_DISCOUNT_PERCENTAGE.getErrorCode());
+    }
+
+    private void testInvalidCheckout(String toolCode, int rentalDays, int discountPercent,
+                                     LocalDate checkoutDate, int expectedErrorCode) throws Exception {
+        CheckoutRequest request = new CheckoutRequest(toolCode, rentalDays, discountPercent,
+                checkoutDate);
+
+        mockMvc.perform(post("/api/rentals")
+                        .content(objectMapper.writeValueAsString(request))
+                .contentType("application/json"))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.resultCode", equalTo(expectedErrorCode)))
+                .andExpect(jsonPath("$.message", not(emptyOrNullString())));
+    }
 }
