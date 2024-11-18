6772cf11acdf393d6e1815988db8a3cbb2ec8c58
Troy Landers <tlanders@landersconsulting.biz>
2024-11-18 17:19:58 -0600

    started work on demo UI.

--- src/main/java/com/tooldepot/pos/ui/CheckoutController.java
+++ src/main/java/com/tooldepot/pos/ui/CheckoutController.java
@@ -1,14 +1,65 @@
 package com.tooldepot.pos.ui;
 
+import com.tooldepot.pos.domain.RentalTransaction;
+import com.tooldepot.pos.service.CheckoutService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 @Controller
 @RequestMapping("/ui")
 public class CheckoutController {
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
     @GetMapping("/checkout")
-    public String showCheckout() {
-        return "ui/checkout";
+    public ModelAndView showCheckout() {
+        log.debug("GET /ui/checkout");
+        return new ModelAndView("ui/checkout", "checkoutFormBean", new CheckoutFormBean());
+    }
+
+    final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yy");
+
+    @PostMapping("/checkout")
+    public ModelAndView postCheckout(@Valid @ModelAttribute("checkoutFormBean") CheckoutFormBean checkoutFormBean,
+                                     BindingResult errors) {
+        log.debug("POST /ui/checkout, bean={}", checkoutFormBean);
+
+        LocalDate checkoutDate = null;
+        if(!errors.hasErrors()) {
+            try {
+                checkoutDate = LocalDate.parse(checkoutFormBean.getCheckoutDate(), dateFormatter);
+            } catch (Exception e) {
+                errors.rejectValue("checkoutDate", "checkoutDate.invalid", "Please enter a valid checkout date (MM/dd/yy)");
+            }
+        }
+
+        if(errors.hasErrors()) {
+            log.debug("Validation errors: {}", errors);
+            return new ModelAndView("ui/checkout", "checkoutFormBean", checkoutFormBean);
+        }
+
+        try {
+            RentalTransaction rentalTransaction = checkoutService.checkout(checkoutFormBean.getToolCode(),
+                    checkoutFormBean.getRentalDays(),
+                    checkoutFormBean.getDiscountPercent(), checkoutDate);
+
+            return new ModelAndView("ui/agreement", "rentalTransaction", rentalTransaction);
+        } catch(Exception e) {
+            log.error("Error checking out", e);
+        }
+
+        return new ModelAndView("ui/checkout", "checkoutFormBean", checkoutFormBean);
     }
 }
---
+++ src/main/java/com/tooldepot/pos/ui/CheckoutFormBean.java
@@ -0,0 +1,25 @@
+package com.tooldepot.pos.ui;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+public class CheckoutFormBean {
+    @NotEmpty(message = "Please enter a valid 4-character tool code")
+    @Pattern(regexp = "^[A-Za-z]{4}$", message = "Please enter a valid 4-character tool code")
+    private String toolCode;
+
+    @Min(value = 1, message = "Rental days must be at least 1")
+    private int rentalDays = 1;
+
+    @Min(value = 0, message = "Discount percent must be at least 0 and not more than 100")
+    @Max(value = 100, message = "Discount percent must be at least 0 and not more than 100")
+    private int discountPercent;
+
+    @NotEmpty(message = "Please enter a valid checkout date (MM/dd/yy)")
+    @Pattern(regexp = "^\\d\\d/\\d\\d/\\d\\d$", message = "Please enter a valid checkout date (MM/dd/yy)")
+    private String checkoutDate;
+}
